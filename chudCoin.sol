36e4e2457ca46a5804819d1e42e26db1d075ad2c
chud69420 <139007801+chud69420@users.noreply.github.com>
2023-07-08 19:36:29 +0100

    Add files via upload

---
+++ chudCoin.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: MIT
+
+//         .75PP5?: :557  ^557 !55^  755: J5555Y7:  ^J55557.      :?5PP57.   :555~   !557  ~55^     7555^ :555?   :555~   !55^ ~Y5Y~ J555555?     ^557.5555555P?
+//        ^#@#?7P#5.^@@G::?@@5 Y@@!  5@@~ #@@??B@&^.#@#?YGP~     !@@G7?G#J   G@#@&:  J@@@P.7@@!     5@@@G 5@@@G  .B@#@#:  Y@@?5@@5~  #@@J?JJ!     ~@@5 ?J5@@#JJ!
+//        J@@7   :  ^@@&###@@Y Y@@~  Y@@~ B@&  ~@@Y !5G###G!     G@@:   .   5@#^G@B. J@@P@#P@@!     5@PP@J@G5@P  5@#^B@G  J@@@@@@7   #@@PPPP~     ~@@5   :@@P   
+//        ^#@B!!5@B:^@@P  7@@5 !@@P!!#@&: #@@77G@@~^GBY^7@@G     7@@5!!G&5 J@@#GB@@P J@@^~#@@@!     5@P^@@@~5@G J@@#GB@@5 Y@@5:Y@@5. #@@?777!     ~@@5   ^@@G   
+//         :?PGGGY^ :PP?  ^PP7  ~YGGGPJ:  YPPPP5J^  !5GPPP?.      ^JPGGPJ::PPJ...?PP^!PP: .YPP^     7GJ ?PJ ?G?:PPJ...?PP^!PP^  ?PP?.YPPPPPPY     ^PP7   :PPJ   
+//                                                                                                                                                              
+//                                                                                                                                                              
+//                                                                                                                                                              
+//                                                                      ^~~~~~~~^  .^!!~:     :~!!^.                                                            
+//                                                                      5#B@@@##P.5&&BG&&P^ ^G@&GB@&Y.                                                          
+//                                                                        .B@&:  Y@@7  ^@@G #@#:  ?@@?                                                          
+//                                                                         B@&.  J@@?  ~@@P B@&^  J@@7                                                          
+//                                                                         B@#.   J#&BB&#5: :P&&B#&B?                                                           
+//                                                                         :::      :^^:.     .:^^:                                                             
+//                                                                                                                                                              
+//                                                                                                                                                                                                                                                     
+//                                                                                                                                                      
+//                                                                                                                                                      
+//                                                                                  ....                                                                
+//                                                             .:7Y5GGPPPPPPGGGGGBBB####BP5YJ?!^:.                                                      
+//                                                          :!5#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BPBBPPY?!^.                                           
+//                                                      :^?P&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#GY!                                        
+//                                                  :!YB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@GY?^.                                   
+//                                              .~YB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#PJ??^                              
+//                                           :!5#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@G                              
+//                                         !G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5.                            
+//                                       7#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@####&@@@@&#@@@@@@@@@@@@&&@@@@@B~                           
+//                                       B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#P?^....:^~!~:.~!~YGGGGGBBP^^G@@@@@&:                          
+//                                     ^G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGGPPPP5555555Y7^.                             7#@@@Y                           
+//                                    .B@@@@@@@@@@@@@@@@@@@@@@@@@&BY7^:.                                                .7G@P                           
+//                                    :&@@@@@@@@@@@@@@@@@@@@@@@P!.                                                         ^BY                          
+//                                    .B@@@@@@@@@@@@@@@@@@@@@@?                                                             ~&~                         
+//                                     ?@@@@@@@@@@@@@@@@@@@@#!                                                               YB:                        
+//                                     ?@@@@@@@@@@@@@@@@@@@J.                          ~                                      Y#^                       
+//                                     B@@@@@@@@@@@@@@@@@@@Y                           ?7                   .                  5B.                      
+//                                    ^&@@@@@@@@@@@@@@@@@@B^                            7J.                .Y.                 :@?                      
+//                                    7@@@@@@@@@@@@@@@@@@&:                              ~J~               ~J                   7@^                     
+//                                    7@@@@@@@@@@@@@@@@@@J                                ^P               J7                   .&J                     
+//                                    !@@@@@@@@@@@@@@@@@G                                  Y!              P^                    PG                     
+//                                    ~@@@@@@@@@@@@@@@@@!                                  ~5             .~                     ?&.                    
+//                                    ~@JG@@@@@@@@@@@@@5                                    5:            Y!                     J#.                    
+//                                    J&: P@@@@@@@@@@@@~         J55J?!^..                                5&^                    Y#.                    
+//                                    GP  .B@@@@@@@@@@Y          J@@@@@@&#GY!^.              .            :&5             .^75GBJB5                     
+//                                   .#Y   ~&@@@@@@@&?            ?5G#&@@@@@@@#GPJ!:       .J7             BP       .~7J5G&@@@@B?@7                     
+//                                   .#J    !#@@@@@G:                .^7YPGBGB&@@@@&BGPP5J77~              G#Y   ~!P#@@@@@@&B5! 7@^                     
+//                                   ^@?     !G&@BY^ .           !GGPGB#&BGGB#&&@@@@@@@@@@#^               BBJ?!P@@@@&#PJ!:.    P@^                     
+//                                   7@~  .JB&BGBGGBBBBG5??????JJ#@G~^^::....:^~7JY555PB##@@B?.           ^@G.7P@@@@@B5YJJYYYYYY@@5~                    
+//                                   Y@^  ^@@!.    .:^7YPPPPPPP5P@@~                    ..!P@@P.:~!7??!.  :&GG@GJJJJJJYYYYYYYY5YG@@B.                   
+//                                   5&:  !@#                   ^&@~ .^^!~~^^^^^.           Y@@#&#GPPG#BGY7&&&@~                ~@@G                    
+//                                   5&:  7@@Y^                  B@^ .P@PJJJG@@@#P7^.       ^&@#~.    .:!5#@@@G      .:::^:     ?@@P                    
+//                                   J&.   ~JP?                 .B#. ^&Y   !@@@@@@@@B5?~.   .&@!          .5@@P?JJYPB&&&&P?     J@@J                    
+//                                   7&:                        ~@B  .YG5^ ?@@@@@@@@?^5&7   :&@^           ~@@5!~^J@@@@@@P      5@@J                    
+//                                   ~@!                        ~@#.   .?G55B###B14885GY    ~@&.           ~@@J^^^7@@&&#&5      P@@J                    
+//                                   ~@!                        ^&@BPY7^..:...!~~~~~!.      5@P            ^@&JYYJ69420Y&J      Y@@J                    
+//                                   .BY                         .~?Y5#&#BBPP5PPPP55YJ7?YPGG&@7            ~@@5?5Y?7!^~7?~~!!!77P@@!                    
+//                                    7#.                              :^^!777?JJJJJJYYJB&?!~:             .Y@@BGB#&&BBB&@@&@@@@@&5.                    
+//                                    :&7                                               GY                   B#.  :^!??!~~^:^^^^BP                      
+//                                     PP                                              ~&^                   Y@^               .&?                      
+//                                     ?B                                              ?#                    !@!               :&7                      
+//                                     ~&^                                             BY                    :#G.              :&7                      
+//                                     :#~                                            ~&:                     ~@7              !@^                      
+//                                     :&~                                            5B~~7?:                 :&P              Y#.                      
+//                                      B?                                           :&@YJ?!.     ..          7@7             ^@?                       
+//                                      P5                                           5##J.    .!5B##?.   :^~~J@Y             :BP                        
+//                                      GP                                          7@~:P#GY~ ?#B#&&5.  7&@@@@@~            ?B?                         
+//                                      GP                                         ~@Y   :?5J.   .::    JY?!~~&?          .GG:                          
+//                                      GP                                         B&:                       :@Y         :GG.                           
+//                                      B5            ^^                          7&~       .....            .B5        !#J                             
+//                                      B5            ^G.                        ~@7  .^7JJ5P5555P5J^^~7??!:  5B       ~@7                              
+//                                      B5             Y!                       ^&J.^J##57!^     .:YGYJ?!!?GY ?&:      GG                               
+//                                     .#Y             Y!                      :#57B@P7?Y555Y777777!~^^^^~^7@G^&J    ~GP:                               
+//                                     !@!             P~                      GB..~P5?^   :~7777777JJJJJ?J5B@YY#.  J#!                                 
+//                                    ~#5             .G:                      7^    ^75P5!.            ~J55?!:^@! PB:                                  
+//                                   .#5             .Y?                                .!Y55P5?^   :~JP57:     ^!B5.                                   
+//                                   ?&^             :~                                      .^?PYJ55J7:        !&?                                     
+//                                   Y#                                                     .~~~!7JJ!:         Y#!                                      
+//                                  ~&J                                                    :~^...   :Y       .GG:                                       
+//                                 Y@J                                                               .    .^JBG:                                        
+//                               .5&7               :~7!                                             .^?PBBPJ!                                          
+//                               P&^             ^5P5Y?~                                        .7JYG##GY!:                                             
+//                             !GB^             Y#?.                                          .?##B@J~.                                                 
+//                          ^!PG!             .P#^                                          .?&G^ ^&?                                                   
+//                     ...~GB57            ..?B5:                                    :~~~7YYBB?.   !&Y~~~:.                                             
+//               .^~7J5P5YPG5YJ.          !55P~                                      ^JJ?J5!:       ^?JJY5P5YYYYYYYYYYYYJJ??!^.                         
+//             7PGPYJ7^.                                                                                  .::::::::^^^^^~!!7YPGPYY!~^
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract chudCoin is ERC20 {
+    address payable public owner;
+    uint256 private _totalSupply = 148869420 * (10 ** 18);
+    uint256 private _tokenPrice = 200000 * (10 ** 18);
+
+    constructor() ERC20("chudCoin", "CHUD")  {
+        owner = payable(msg.sender);
+        _mint(owner, _totalSupply);
+    }
+    
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No balance to withdraw");
+        payable(msg.sender).transfer(balance);
+    }
+
+    function setTokenPrice(uint256 newTokenPrice) external onlyOwner {
+        require(newTokenPrice > 0, "Token price should be greater than 0");
+        _tokenPrice = newTokenPrice;
+    }
+
+    function getTokenPrice() external view returns (uint256) {
+        return _tokenPrice;
+    }
+
+    function burn(uint256 amount) external {
+        require(amount > 0, "Amount to burn should be greater than 0");
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
+        _burn(msg.sender, amount);
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), allowance(sender, _msgSender()) - amount);
+        return true;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address.");
+        owner = payable(newOwner);
+    }
+
+    function renounceOwnership() external onlyOwner {
+        owner = payable(address(0));
+    }
+}
